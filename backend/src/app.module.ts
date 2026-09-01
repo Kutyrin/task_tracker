7e11959bf0c91346daf9175c5f0978c49340b015
Egor <mrdgora@mail.ru>
2026-09-01 16:34:21 +0300

    project set-up

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { HealthController } from './health.controller';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [AppController, HealthController],
 })
 export class AppModule {}
